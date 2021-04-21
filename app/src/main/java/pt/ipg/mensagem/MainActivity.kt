13a1278feb11b1a5606f839d5923456d7e954041
Leonardo Marques <leonardo2000marques@gmail.com>
2021-04-21 10:50:16 +0100

    Obter a mensagem introduzida pelo utilizador

--- app/src/main/java/pt/ipg/mensagem/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.kt
@@ -1,11 +1,25 @@
 package pt.ipg.mensagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun enviaMensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+
+        startActivity(intent)
+
     }
 }
