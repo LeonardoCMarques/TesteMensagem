cfcd2fdd440ad4465e2965719efbfeaf6266152e
Leonardo Marques <leonardo2000marques@gmail.com>
2021-04-21 10:58:21 +0100

    Mostrar a mensagem recebida

--- app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
@@ -2,10 +2,17 @@ package pt.ipg.mensagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.TextValueSanitizer
+import android.widget.TextView
 
 class MostraMensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem)
+
+        val mensagem = intent.getStringExtra("Mensagem")
+
+        val textViewMensagem = findViewById<TextView>(R.id.textViewMensagem)
+        textViewMensagem.setText(mensagem)
     }
 }
